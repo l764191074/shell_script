08716cff2ddf3c330374aab756e5b1e299a0057b
Administrator <764191074@qq.com>
2017-09-29 17:16:29 +0800

    change bug

--- ubuntu14_install_python3.6.2.sh
+++ ubuntu14_install_python3.6.2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt-get install python-software-properties -y && \
 apt-get install software-properties-common -y && \
-sudo add-apt-repository ppa:jonathonf/python-3.6 && \
-sudo apt-get update && \
-sudo apt-get install python3.6 -y && \
+apt-get install wget -y && \
+add-apt-repository ppa:jonathonf/python-3.6 && \
+apt-get update && \
+apt-get install python3.6 -y && \
 wget https://bootstrap.pypa.io/get-pip.py && \
 python3.6 get-pip.py
