79b7930212a92ff2b34429c3b1dbea1cc0368cd4
l764191074 <764191074@qq.com>
2018-01-26 16:42:52 +0800

    Update ubuntu_install_make_python.sh

--- ubuntu_install_make_python.sh
+++ ubuntu_install_make_python.sh
@@ -1,11 +1,11 @@
-sed -i 's\archive.ubuntu.com\mirrors.aliyun.com\g' /etc/apt/sources.list
-apt-get update
+sudo sed -i 's\archive.ubuntu.com\mirrors.aliyun.com\g' /etc/apt/sources.list
+sudo apt-get update
 cd /home/
-apt-get install gcc  make zlib1g-dev -y
-apt-get install  libbz2-dev libsqlite3-dev  libxml2-dev  libffi-dev libssl-dev -y
-apt install wget -y
+sudo apt-get install gcc  make zlib1g-dev -y
+sudo apt-get install  libbz2-dev libsqlite3-dev  libxml2-dev  libffi-dev libssl-dev -y
+sudo apt install wget -y
 wget http://mirrors.sohu.com/python/3.6.2/Python-3.6.2.tgz
-tar -xvf Python-3.6.2.tgz 
+tar -xvf Python-3.6.2.tgz
 cd Python-3.6.2
 ./configure --prefix=/home/usr/python36/
 make
@@ -18,7 +18,10 @@ trusted-host=mirrors.aliyun.com
 index-url=http://mirrors.aliyun.com/pypi/simple/
 EOF
 
-apt-get install libmysqlclient-dev -y
+sudo apt-get install libmysqlclient-dev -y
+ln -s /home/usr/python36/bin/pip3 /usr/local/sbin/pip36
+ln -s /home/usr/python36/bin/python3 /usr/local/sbin/python36
+
 ./pip3 install mysqlclient
 ./pip3 install Django
 ./pip3 install gunicorn
