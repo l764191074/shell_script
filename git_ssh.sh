fa1d05a61b89097f6cadb4557d3ff9ffdbd71bea
l764191074 <764191074@qq.com>
2018-08-20 15:18:24 +0800

    git 初始化脚步

---
+++ git_ssh.sh
@@ -0,0 +1,8 @@
+chown 755 ~/.ssh/ &&
+chown -R  644 ~/.ssh/* &&
+chown 600  ~/.ssh/id_rsa
+
+git config --global user.email "764191074@qq.com"
+git config --global user.name "l764191074"
+git config --global core.editor "vim"
+
